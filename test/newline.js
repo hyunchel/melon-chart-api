7962c69df9dcc49629cbb4c5488f223bd0a4a02f
Hyunchel Kim <hyunchel.inbox@gmail.com>
2017-08-30 09:22:44 -0500

    Update JSON content, add new JSON data
    There was a change in song title for 2016 JOSN data.
    It is updated with this commit.
    
    The new JSON data tests elimination of unwanted "19금" and newlines.

--- test/newline.js
+++ test/newline.js
@@ -1,12 +1,12 @@
 import test from 'ava';
 import Melon from '..';
-import JSON_WEEK_OF_2016_12_25 from './week_of_2016_12_25';
-import JSON_MONTH_OF_2016_12_25 from './month_of_2016_12_25';
+import JSON_WEEK_OF_2017_06_06 from './week_of_2017_06_06';
+import JSON_MONTH_OF_2017_06_06 from './month_of_2017_06_06';
 
 test.beforeEach(t => {
   const options = {
-    date: '2016/12/25',
-    cutLine: 10
+    date: '2017/06/06',
+    cutLine: 50
   };
   const melon = Melon(options.date, options);
   t.context.options = options;
@@ -20,18 +20,18 @@ test('content - daily', t => {
   //       as Melon Music Chart does not provide previous daily charts.
   //       Daily chart will always fetch today's chart.
   return t.context.daily.then(chart => {
-    t.notDeepEqual(chart, JSON_WEEK_OF_2016_12_25);
+    t.notDeepEqual(chart, JSON_WEEK_OF_2017_06_06);
   });
 });
 
 test('content - weekly', t => {
   return t.context.weekly.then(chart => {
-    t.deepEqual(chart, JSON_WEEK_OF_2016_12_25);
+    t.deepEqual(chart, JSON_WEEK_OF_2017_06_06);
   });
 });
 
 test('content - monthly', t => {
   return t.context.monthly.then(chart => {
-    t.deepEqual(chart, JSON_MONTH_OF_2016_12_25);
+    t.deepEqual(chart, JSON_MONTH_OF_2017_06_06);
   });
 });
